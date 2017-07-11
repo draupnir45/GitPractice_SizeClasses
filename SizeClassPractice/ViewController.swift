59f4f3717ac6218033614de117250bcd97a0b0ae
Jongchan Park <draupnir45@gmail.com>
2017-07-11 10:39:32 +0900

    Update initial project.

---
+++ SizeClassPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassPractice
+//
+//  Created by 박종찬 on 2017. 7. 11..
+//  Copyright © 2017년 Jongchan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
